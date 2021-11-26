dfa6c95a9ce9248c677236ddb805363dce872a0a
pete rodrigue <edwardpierrerodrigue@gmail.com>
2021-11-26 15:45:57 -0500

    modified to hide secret key

--- todo_site_top_level/todo_site/settings.py
+++ todo_site_top_level/todo_site/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c3t1i*lqhpjzl7l(zxm6g-x9-mvcyc55$0#3a6qonkjt6l#2lx'
+# SECRET_KEY = 'django-insecure-c3t1i*lqhpjzl7l(zxm6g-x9-mvcyc55$0#3a6qonkjt6l#2lx'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
